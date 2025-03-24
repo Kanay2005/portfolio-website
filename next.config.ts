da02685b5fda516f619ef7bc47bc024c605ff990
Kanay Gupta <z5477443@ad.unsw.edu.au>
2025-03-25 01:48:07 +1100

    testingImagePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-website/' : '',
   /* config options here */
 };
 
